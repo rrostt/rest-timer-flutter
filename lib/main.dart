0f0db206b32cf0d427a7d48ec8cb7959ddff2fd0
Mattias Rost <rrostt@gmail.com>
2019-02-11 22:08:25 +0100

    Format document

--- lib/main.dart
+++ lib/main.dart
@@ -84,19 +84,19 @@ class MyContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return RefreshIndicator(
       child: SingleChildScrollView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        child: Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                ExercisesWidget(),
-                HintText(),
-                TimerWidget(),
-                TotalTimeWidget(),
-                SplitTimes(),
-              ],
-            ))),
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  ExercisesWidget(),
+                  HintText(),
+                  TimerWidget(),
+                  TotalTimeWidget(),
+                  SplitTimes(),
+                ],
+              ))),
       onRefresh: () async {
         ScopedModel.of<TimerModel>(context).reset();
       },
@@ -121,48 +121,105 @@ class Settings extends StatelessWidget {
 class RestPeriodSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<TimerModel>(builder: (context, child, model) {
-      var onChanged = (value) { model.setRestPeriod(value.toInt()); };
-      return _buildCard(model.restPeriod, onChanged);
-    },);
+    return ScopedModelDescendant<TimerModel>(
+      builder: (context, child, model) {
+        var onChanged = (value) {
+          model.setRestPeriod(value.toInt());
+        };
+        return _buildCard(model.restPeriod, onChanged);
+      },
+    );
   }
 
   Widget _buildCard(int restPeriod, onChanged) {
-    return Card(child: Row(children: <Widget>[
-      Padding(padding: EdgeInsets.all(16.0), child: Column(children: <Widget>[Icon(Icons.timer),Text('${restPeriod}s', style: TextStyle(fontSize: 10))]),),
-      Expanded(child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-        Padding(padding: EdgeInsets.only(left: 16), child: Text('Rest period', style: TextStyle(fontSize: 16)),),
-        Slider(value: restPeriod.toDouble(), onChanged: onChanged, min: 0, max: 180,),
-        Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          FlatButton(child: Text('60S'), textColor: Colors.blue[700], onPressed: () { onChanged(60); },),
-          FlatButton(child: Text('90S'), textColor: Colors.blue[700], onPressed: () { onChanged(90); },),
-          FlatButton(child: Text('2 MINS'), textColor: Colors.blue[700], onPressed: () { onChanged(120); },),
-        ],)
-      ],))
-    ],),);
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(children: <Widget>[
+              Icon(Icons.timer),
+              Text('${restPeriod}s', style: TextStyle(fontSize: 10))
+            ]),
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 16),
+                child: Text('Rest period', style: TextStyle(fontSize: 16)),
+              ),
+              Slider(
+                value: restPeriod.toDouble(),
+                onChanged: onChanged,
+                min: 0,
+                max: 180,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('60S'),
+                    textColor: Colors.blue[700],
+                    onPressed: () {
+                      onChanged(60);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('90S'),
+                    textColor: Colors.blue[700],
+                    onPressed: () {
+                      onChanged(90);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('2 MINS'),
+                    textColor: Colors.blue[700],
+                    onPressed: () {
+                      onChanged(120);
+                    },
+                  ),
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
 
 class ExerciseSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<TimerModel>(builder: (context, child, model) {
-      List <Widget> exercises = model.exercises.map<Widget>((exercise) =>
-            ListTile(
-              title: Text(exercise['text']),
-              leading: Icon(Icons.subject),
-              trailing: IconButton(icon: Icon(Icons.delete),onPressed: () => model.removeExercise(exercise['text']),),
-            )
-          ).toList();
-      List<Widget> children = List.from(exercises);
-      children.add(AddExercise());
-      return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-        Padding(padding: EdgeInsets.all(16), child: Text('Exercises', style: TextStyle(fontSize: 16)),),
-        Card(child: Column(
-          children: children
-        ),),
-      ]);
-    },);
+    return ScopedModelDescendant<TimerModel>(
+      builder: (context, child, model) {
+        List<Widget> exercises = model.exercises
+            .map<Widget>((exercise) => ListTile(
+                  title: Text(exercise['text']),
+                  leading: Icon(Icons.subject),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => model.removeExercise(exercise['text']),
+                  ),
+                ))
+            .toList();
+        List<Widget> children = List.from(exercises);
+        children.add(AddExercise());
+        return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Text('Exercises', style: TextStyle(fontSize: 16)),
+              ),
+              Card(
+                child: Column(children: children),
+              ),
+            ]);
+      },
+    );
   }
 }
 
@@ -182,25 +239,36 @@ class AddExerciseState extends State<AddExercise> {
     return Padding(
       padding: EdgeInsets.only(left: 16, right: 16),
       child: Column(children: <Widget>[
-        Row(children: <Widget>[
-          Expanded(child: TextField(controller: controller,),),
-          IconButton(icon: Icon(Icons.add), onPressed: () {
-            if (controller.text != '') {
-              if (!ScopedModel.of<TimerModel>(context).addExercise(controller.text)) {
-                setState(() {
-                  error = '"${controller.text}" already exists';
-                });
-              } else {
-                setState(() {
-                  error = null;
-                });
-                controller.text = '';
-              }
-            }
-            FocusScope.of(context).requestFocus(new FocusNode());
-          }),
-        ],),
-        error != null ? Text(error, style: TextStyle(color: Colors.red[700])) : Container()
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                controller: controller,
+              ),
+            ),
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  if (controller.text != '') {
+                    if (!ScopedModel.of<TimerModel>(context)
+                        .addExercise(controller.text)) {
+                      setState(() {
+                        error = '"${controller.text}" already exists';
+                      });
+                    } else {
+                      setState(() {
+                        error = null;
+                      });
+                      controller.text = '';
+                    }
+                  }
+                  FocusScope.of(context).requestFocus(new FocusNode());
+                }),
+          ],
+        ),
+        error != null
+            ? Text(error, style: TextStyle(color: Colors.red[700]))
+            : Container()
       ]),
     );
   }
@@ -209,27 +277,29 @@ class AddExerciseState extends State<AddExercise> {
 class HintText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<TimerModel>(builder: (context, child, model) {
-      return Container(
-        margin: EdgeInsets.all(32.0),
-        child: Text(
-          'Press time to start, pull down to reset',
-          style: TextStyle(
-            fontSize: 24,
-            color: model.isStarted ? Color(0) : Color(0xff888888),
-            fontFamily: 'monospace'
-          ),)
-      );
-    },);
+    return ScopedModelDescendant<TimerModel>(
+      builder: (context, child, model) {
+        return Container(
+            margin: EdgeInsets.all(32.0),
+            child: Text(
+              'Press time to start, pull down to reset',
+              style: TextStyle(
+                  fontSize: 24,
+                  color: model.isStarted ? Color(0) : Color(0xff888888),
+                  fontFamily: 'monospace'),
+            ));
+      },
+    );
   }
-
 }
 
 class ExercisesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<TimerModel>(builder: (context, child, model) {
-      return ExerciseRow(exercises: model.exercises,);
+      return ExerciseRow(
+        exercises: model.exercises,
+      );
     });
   }
 }
@@ -240,14 +310,17 @@ class ExerciseRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(scrollDirection: Axis.horizontal, child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: exercises.map<Widget>((e) => ExerciseChip(text: e['text'], count: e['count'])).toList(),
-    ));
-
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: exercises
+              .map<Widget>(
+                  (e) => ExerciseChip(text: e['text'], count: e['count']))
+              .toList(),
+        ));
   }
-
 }
 
 typedef void ExerciseChipOnTap(String name);
@@ -259,20 +332,20 @@ class ExerciseChip extends StatelessWidget {
   ExerciseChip({this.text, this.count});
 
   @override
-  Widget build(BuildContext context) =>
-    GestureDetector(
-      onTap: () {
-        ScopedModel.of<TimerModel>(context).incExercise(text);
-      },
-      child:
-        Chip(
-          label: Text(text),
-          avatar: CircleAvatar(
-              backgroundColor: Colors.red[700],
-              child: Text('$count', style: TextStyle(fontSize: 12, color: Colors.white, fontWeight: FontWeight.bold))
-          )
-        ),
-    );
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: () {
+          ScopedModel.of<TimerModel>(context).incExercise(text);
+        },
+        child: Chip(
+            label: Text(text),
+            avatar: CircleAvatar(
+                backgroundColor: Colors.red[700],
+                child: Text('$count',
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)))),
+      );
 }
 
 class TotalTimeWidget extends StatelessWidget {
@@ -330,7 +403,8 @@ class TimerWidget extends StatelessWidget {
           onTap: () {
             model.split();
           },
-          child: TimerText(model.currentSplitTime, model.currentSplitTime >= model.restPeriod * 1000));
+          child: TimerText(model.currentSplitTime,
+              model.currentSplitTime >= model.restPeriod * 1000));
     });
   }
 }
@@ -348,7 +422,9 @@ class TimerText extends StatelessWidget {
         children: <Widget>[
           Text(formatTime(time),
               style: TextStyle(
-                  fontSize: 72, color: restOver ? Colors.red[700] : Colors.black, fontFamily: 'monospace'))
+                  fontSize: 72,
+                  color: restOver ? Colors.red[700] : Colors.black,
+                  fontFamily: 'monospace'))
         ]);
   }
 }
